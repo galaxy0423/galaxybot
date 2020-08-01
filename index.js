66c737864a402c9500406d1b9d6768b19b2031a7
galaxy0423 <gg.galaxy0423@gmail.com>
2020-08-01 22:07:43 +0900

    !re

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ client.on('message', (message) => {
     var duration = moment.duration(client.uptime).format(" D [일], H [시간], m [분], s [초]");
     embed.setColor('#186de6')
     embed.setAuthor('server info of 콜라곰 BOT', img)
-    embed.setFooter(`콜라곰 BOT ❤️`)
+    embed.setFooter(`우주 Bot`)
     embed.addBlankField()
     embed.addField('RAM usage',    `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true);
     embed.addField('running time', `${duration}`, true);
